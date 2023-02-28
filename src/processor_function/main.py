703be9d979a8821ab24c9f09ce3c0c4ab4d7c7bc
atiagosoares <atiagosoares@gmail.com>
2023-02-27 23:09:10 -0300

    Modified the function to correctly handle pubsub data

--- src/processor_function/main.py
+++ src/processor_function/main.py
@@ -1,5 +1,10 @@
 import functions_framework
+from base64 import b64decode
 # Dummy cloud function
-@functions_framework.http
-def handler(request):
+@functions_framework.cloud_event
+def handler(cloud_event):
+    # Print the event data
+    base64_data = cloud_event.data['message']['data']
+    data = b64decode(base64_data).decode('utf-8')
+    print(f"Received message: {data}")
     return "Hello world"
